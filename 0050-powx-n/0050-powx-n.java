7944e89354fb480ac566bc0ce0fa73d70de11bda
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2023-08-12 17:56:21 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (6.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,8 +1,23 @@
-import java.lang.Math;
-
 class Solution {
-    public double myPow(double x, int n) {
-       double result = Math.pow(x,n);
+    public double myPow(double x, long n) {
+        double result = 1.0;
+        double mul = 1.0;
+        if( n == 0){
+            return 1;
+        }
+        if (n < 0) {
+            n = -1 * n;
+            x = 1.0 / x;
+        }
+        while(n!=0){
+            if (n % 2 == 1) {
+                result = result * x;
+                n -= 1;
+            }
+            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x = x * x;
+            n = n / 2;
+        }
         return result;
     }
 }
