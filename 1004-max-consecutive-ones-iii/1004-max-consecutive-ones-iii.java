0f0fbca8fc920010ee0c53ffad0fb293bf572d36
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-04-26 12:33:02 +0530

    Time: 4 ms (26.26%), Space: 46.9 MB (52.87%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -16,7 +16,7 @@ class Solution {
         int l = 0, r = 0, zeros = 0, len = 0;
         while(r<nums.length){
             if(nums[r] == 0) zeros++;
-             while(zeros > k){
+             if(zeros > k){
                  if(nums[l] == 0) zeros--;
                  l++;
              }   
