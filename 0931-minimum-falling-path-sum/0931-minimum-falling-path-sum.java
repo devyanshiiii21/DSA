b78024c303bd5d8ce34a294cc732312c69a78f1c
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-07-11 17:18:06 +0530

    Time: 4 ms (74.39%), Space: 44.9 MB (76.87%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,41 +1,89 @@
-// Tabulation
+// Space Optimization
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int min = Integer.MAX_VALUE;
         
-        int dp[][] = new int[n][n];
-        for(int[] row : dp){
-            Arrays.fill(row, 0);
-        }
-// Base case
+        int prev[] = new int[n];
+        Arrays.fill(prev, 0);
+        
+        int curr[] = new int[n];
+        Arrays.fill(curr, 0);
+        
+        // Base case
         for(int j = 0; j<n; j++){
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
 
         for(int i = 1; i < n; i++){
             for(int j = 0; j < n; j++){
-                int st = matrix[i][j] + dp[i-1][j];
+                int st = matrix[i][j] + prev[j];
                 
                 int leftdg = matrix[i][j];
-                if(j-1 >= 0) leftdg += dp[i-1][j-1];
+                if(j-1 >= 0) leftdg += prev[j-1];
                 else leftdg = Integer.MAX_VALUE;
 
                 int rightdg = matrix[i][j];
-                if(j+1 < n) rightdg += dp[i-1][j+1];
+                if(j+1 < n) rightdg += prev[j+1];
                 else rightdg = Integer.MAX_VALUE;
 
-                dp[i][j] = Math.min(st, Math.min(leftdg, rightdg));
+                curr[j] = Math.min(st, Math.min(leftdg, rightdg));
+            }
+            for(int j = 0; j < n; j++) {
+                prev[j] = curr[j];
             }
         }
+        
         for(int j = 0; j < n; j++){
-                min = Math.min(min, dp[n-1][j]);
-            }
-
+            min = Math.min(min, prev[j]);
+        }
         return min;
     }
 
 }
+
+
+
+
+// // Tabulation
+// class Solution {
+//     public int minFallingPathSum(int[][] matrix) {
+//         int n = matrix.length;
+//         int min = Integer.MAX_VALUE;
+        
+//         int dp[][] = new int[n][n];
+//         for(int[] row : dp){
+//             Arrays.fill(row, 0);
+//         }
+        
+//         // Base case
+//         for(int j = 0; j<n; j++){
+//             dp[0][j] = matrix[0][j];
+//         }
+
+//         for(int i = 1; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 int st = matrix[i][j] + dp[i-1][j];
+                
+//                 int leftdg = matrix[i][j];
+//                 if(j-1 >= 0) leftdg += dp[i-1][j-1];
+//                 else leftdg = Integer.MAX_VALUE;
+
+//                 int rightdg = matrix[i][j];
+//                 if(j+1 < n) rightdg += dp[i-1][j+1];
+//                 else rightdg = Integer.MAX_VALUE;
+
+//                 dp[i][j] = Math.min(st, Math.min(leftdg, rightdg));
+//             }
+//         }
+//         for(int j = 0; j < n; j++){
+//                 min = Math.min(min, dp[n-1][j]);
+//             }
+
+//         return min;
+//     }
+
+// }
 
 
 // Memoization
