6b8acfa543f03449afed679c82add901118e9d0e
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-08-02 19:01:15 +0530

    Time: 6 ms (86.28%), Space: 59.1 MB (65.43%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minSwaps(int[] nums) {
+        int n = nums.length;
+        int windowSize = 0;
+        
+        for(int num : nums){
+            windowSize += num;
+        }
+        
+        int curZeros = 0;
+        for(int i = 0; i < windowSize; i++){
+            if(nums[i] == 0) curZeros++;
+        }
+        
+        int minZeros = curZeros;
+        int start = 0;
+        int end = windowSize -1;
+        while(start < n){
+            if(nums[start] == 0) curZeros--;
+            start++;
+            end++;
+            if(nums[end%n] == 0) curZeros++;
+            
+            minZeros = Math.min(minZeros, curZeros);
+        }
+        
+        return minZeros;
+    }
+}
