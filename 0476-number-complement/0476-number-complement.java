7af20da6603210cef98550f189c30d395a7f69d6
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-08-22 06:03:30 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (6.96%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findComplement(int num) {
+        int len = (int)(Math.log(num) / Math.log(2)) +1;
+        int mask = (1 << len) -1;
+        int ans = num ^ mask;
+        
+        return ans;
+    }
+}
