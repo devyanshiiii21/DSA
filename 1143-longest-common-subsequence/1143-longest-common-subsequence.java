0e7e82043c526f15388ab584669e3b6e3e15828b
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-08-31 23:23:04 -0700

    Time: 22 ms (47.23%), Space: 51.2 MB (10.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,14 +3,14 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
         
-        return lcs(n-1, m-1, text1, text2, dp);
+        return tabulation(text1, text2, dp, n, m);
     }
-    
+//     Memoization
     public int lcs(int i, int j, String s, String t, int[][] dp){
         if(i < 0 || j < 0) return 0;
         
@@ -20,5 +20,26 @@ class Solution {
             return dp[i][j] = 1 + lcs(i-1, j-1, s, t, dp);
         
         return dp[i][j] = 0 + Math.max(lcs(i-1, j, s, t, dp), lcs(i, j-1, s, t, dp));
+    }
+    
+//     Tabulation
+    public int tabulation(String s, String t, int[][] dp, int n, int m){
+//         Base cases
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = 0;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = 0 + Math.max(dp[i-1][j], dp[i][j-1]); 
+            }
+        }
+        return dp[n][m];
     }
 }
