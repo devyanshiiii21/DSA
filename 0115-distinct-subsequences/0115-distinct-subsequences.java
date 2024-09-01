a78df48bc3c1d9c6d319df07931d908f59712377
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-09-01 05:44:18 -0700

    Time: 14 ms (64.30%), Space: 49.1 MB (26.43%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,13 +3,27 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 1;
         }
         
-        return memoization(n-1, m-1, s, t, dp);
+        for(int j = 1; j <= m; j++){
+            dp[0][j] = 0;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+    
+        
+        return dp[n][m];
     }
     
     public int memoization(int i, int j, String s, String t, int[][] dp){
@@ -23,4 +37,5 @@ class Solution {
         
         return dp[i][j] = memoization(i-1, j, s, t, dp);
     }
+
 }
