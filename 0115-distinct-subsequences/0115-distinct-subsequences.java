a1bf2ace52444772d6e7a92b6d4668444033dade
Devyanshi Srivastava <103577817+devyanshiiii21@users.noreply.github.com>
2024-09-01 05:14:14 -0700

    Time: 11 ms (90.49%), Space: 49.2 MB (18.93%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        
+        int[][] dp = new int[n][m];
+        
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        
+        return memoization(n-1, m-1, s, t, dp);
+    }
+    
+    public int memoization(int i, int j, String s, String t, int[][] dp){
+        if(j<0) return 1;
+        if(i < 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s.charAt(i) == t.charAt(j))
+            return dp[i][j] = memoization(i-1, j-1, s, t, dp) + memoization(i-1, j, s, t, dp);
+        
+        return dp[i][j] = memoization(i-1, j, s, t, dp);
+    }
+}
